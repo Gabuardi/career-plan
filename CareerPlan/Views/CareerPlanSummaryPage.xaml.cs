56f554bbb331c3459e43ead379325f892ecd7fab
Josué Gabuardi <josuegabuardi@gmail.com>
2021-12-09 23:36:44 -0600

    Create new page

---
+++ CareerPlan/Views/CareerPlanSummaryPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CareerPlan.Views
+{
+    public partial class CareerPlanSummaryPage : ContentPage
+    {
+        public CareerPlanSummaryPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
