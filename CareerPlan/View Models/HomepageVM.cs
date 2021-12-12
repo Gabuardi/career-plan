31a8113f22f184d7ff0bb528d7611561868bcb72
Josué Gabuardi <josuegabuardi@gmail.com>
2021-12-12 15:46:02 -0600

    Add models

---
+++ CareerPlan/View Models/HomepageVM.cs
@@ -0,0 +1,27 @@
+﻿namespace CareerPlan.ViewModels
+{
+    public class HomepageVM
+    {
+        public string CareerPlanName
+        {
+            get { return "Bachillerato en Biotecnología Empresarial Acuatica"; }
+        }
+
+        public int CareerPlanProgress
+        {
+            get { return 50; }
+        }
+
+        public int RemainingCourses
+        {
+            get { return 32;  }
+        }
+
+        public int RemainingMonths
+        {
+            get { return 5; }
+        }
+
+    }
+}
+
