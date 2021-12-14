c6b71f0b124e0f9d08a0b77b2280c600c6dda558
Josué Gabuardi <josuegabuardi@gmail.com>
2021-12-14 12:40:33 -0600

    Bind MVVM

--- CareerPlan/Views/ManagePeriodList.xaml.cs
+++ CareerPlan/Views/ManagePeriodList.xaml.cs
@@ -1,30 +1,18 @@
 ﻿using Xamarin.Forms;
-using System.Collections.ObjectModel;
-using CareerPlan.Models;
 using CareerPlan.ViewModels;
 
 namespace CareerPlan.Views
 {
     public partial class ManagePeriodListPage : ContentPage
     {
-        ObservableCollection<Period> periods = new ObservableCollection<Period>();
         private readonly ManagePeriodListVM viewModel;
 
         public ManagePeriodListPage(Models.CareerPlan newCareerPlan)
         {
             InitializeComponent();
-
-            PeriodsList.ItemsSource = periods;
-            periods.Add(new Period { Name="I Cuatrimeste" });
-            periods.Add(new Period { Name= "II Cuatrimeste" });
-            periods.Add(new Period { Name = "III Cuatrimeste" });
-            periods.Add(new Period { Name = "IV Cuatrimeste" });
-
             viewModel = new ManagePeriodListVM(newCareerPlan);
             BindingContext = viewModel;
         }
-
-        public ObservableCollection<Period> Periods { get { return periods; } }
 
         async void Add_Button_Clicked(System.Object sender, System.EventArgs e)
         {
