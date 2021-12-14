05651239ee58dd8f55fed382555e6c99f4a8deb0
Josué Gabuardi <josuegabuardi@gmail.com>
2021-12-14 03:46:51 -0600

    Alternative periods columns background

--- CareerPlan/View Models/CareerPlanBoardVM.cs
+++ CareerPlan/View Models/CareerPlanBoardVM.cs
@@ -1,21 +1,34 @@
 ﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using CareerPlan.Models;
 
 namespace CareerPlan.ViewModels
 {
     public class CareerPlanBoardVM
     {
-        readonly private List<Period> periodsList = new List<Period>
+        private ObservableCollection<Period> periodsList = new ObservableCollection<Period>
         {
-            new Period{ Name="Terra", Months=720 },
+            new Period { Name = "Terra", Months = 720 },
             new Period{ Name="Purgatory", Months=80},
             new Period{ Name="Hell", Months=2160},
             new Period{ Name="Heaven", Months=1}
         };
+        private bool isColumnsEven = false;
 
-        List<Period> PeriodsList
+        public ObservableCollection<Period> PeriodsList
         {
             get { return periodsList; }
         }
+
+        public bool IsColumnsEven
+        {
+            get
+            {
+                bool currentValue = isColumnsEven;
+                isColumnsEven = !currentValue;
+                return currentValue;
+            }
+        }
+
     }
 }
